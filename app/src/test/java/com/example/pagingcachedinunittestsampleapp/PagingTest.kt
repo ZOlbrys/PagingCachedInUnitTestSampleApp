f27872891eebac98116e5e33831c661f90195c5d
Zach Olbrys <ZOlbrys@gmail.com>
2024-02-06 18:38:38 -0500

    Initial commit

---
+++ app/src/test/java/com/example/pagingcachedinunittestsampleapp/PagingTest.kt
@@ -0,0 +1,41 @@
+package com.example.pagingcachedinunittestsampleapp
+
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class PagingTest {
+//    @get:Rule
+//    val coroutineTestRule = CoroutineTestRule()
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `paging cachedIn collect`() = runTest {
+        val pagingData = PagingData.from(listOf(42))
+        val testFlow = flow {
+            emit(pagingData)
+        }
+        var invokeCount = 0
+
+        val scope = TestScope()
+
+        val job = launch {
+            testFlow.cachedIn(scope).collect {
+                invokeCount++
+            }
+        }
+
+        advanceUntilIdle()
+
+        job.cancel()
+
+        assertEquals(1, invokeCount)
+    }
+}
